fbdcb6209ec3c21f5c646a0f12a3f9fabb388214
Michael <mikeishere3@gmail.com>
2018-07-29 02:01:15 -0400

    new mvc application created, scraper copied into own class, running from button press on home page

---
+++ WebScraper/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebScraper.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public Action Scrape()
+        {
+            Scraper myScraper = new Scraper();
+            myScraper.Scrape();
+            return null;
+        }
+    }
+}
